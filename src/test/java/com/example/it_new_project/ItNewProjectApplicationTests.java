95d096bae8b8836cb2ad355dea5e10aa89bcf8de
Elena Iurcenko <lena.iurcenko@mail.ru>
2023-06-15 21:31:17 +0300

    Initial commit

---
+++ src/test/java/com/example/it_new_project/ItNewProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.it_new_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItNewProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
